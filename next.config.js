eafa80c77473c2ebbd32a3aa37ac90a0b56ee5f6
KFuczek <krzy96@interia.pl>
2023-06-13 21:12:04 +0200

    setup deploying

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+    // trim off `<owner>/`
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
+const images = {
+    loader: 'akamai',
+}
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    images: images,
+}
