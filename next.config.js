51a563bf202b4f4cd44f173dada5b92188d13102
KFuczek <krzy96@interia.pl>
2023-06-20 14:08:53 +0200

    add console log in config

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
 let basePath = ''
-
+console.log('is github', isGithubActions)
 if (isGithubActions) {
     // trim off `<owner>/`
     const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
+    console.log('repo ', repo)
     assetPrefix = `/${repo}/`
     basePath = `/${repo}`
 }
